8f0d7ee1f06a5bd1ff83d21f09bed05fd00917f0
mai mahmoud <149944898+mai-xe7@users.noreply.github.com>
2024-05-07 00:10:09 +0300

    Update register.php

--- h/register.php
+++ h/register.php
@@ -1,39 +1,88 @@
-    <?php
+//     <?php
 
-       session_start();
+//        session_start();
 
 
-        if(isset($_POST['register'])){
+//         if(isset($_POST['register'])){
 
-       include 'connection.php';
+//        include 'connection.php';
 
-       $name=$_POST['Username'];
-       $email=$_POST['email'];
-       $password=$_POST['Password'];
+//        $name=$_POST['Username'];
+//        $email=$_POST['email'];
+//        $password=$_POST['Password'];
 
-       $sql = "insert into
-        `user` (`Username`, `email`, `Password`)
-        VALUES ('$name', '$email', '$password')" ;
+//        $sql = "insert into
+//         `user` (`Username`, `email`, `Password`)
+//         VALUES ('$name', '$email', '$password')" ;
 
-       # to perform the query above 
+//        # to perform the query above 
 
-       $query=mysqli_query($conn,$sql);
+//        $query=mysqli_query($conn,$sql);
 
-       if($query){
-        echo " Registrated !!";
-        #go to home code 
-        $_SESSION['Username']=$name;    # to make in page in my web access to this value 
-        header('location:home.php');
-       }
-       else{
-        echo " Try Again Please";
-       }
-     }
-    ?>
+//        if($query){
+//         echo " Registrated !!";
+//         #go to home code 
+//         $_SESSION['Username']=$name;    # to make in page in my web access to this value 
+//         header('location:home.php');
+//        }
+//        else{
+//         echo " Try Again Please";
+//        }
+//      }
+//     ?>
+
+// <html>
+// <head>
+// <title>
+//                         Register
+//                 </title>
+//         </head>
+
+//    <body>
+//         <div class="register">
+                
+//             <form method="POST">
+
+//               <lable class="l"> Registration</lable><br>
+
+//               <lable for="username"> User_Name : </lable> <br>
+//               <input type="text" placeholder="User Name"  name="username" required><br>
+//               <br>
+//               <br>
+//               <lable for="email"> Email : </lable> <br>
+//               <input type="email" placeholder="Email"  name="email" required><br>
+//               <br>
+//               <br>
+//               <lable for="pass"> Password : </lable><br>
+//               <input type="password" placeholder="Password"  name="pass" required><br>
+//               <br>
+//               <br>
+//               <button type=" submit " name="register">Register</button>
+//               <br>
+//         </form>
+// </div>
+
+
+// <?php
+// if(isset($_POST['reg'])){
+//         header('location:login.php');
+// }
+// ?>
+
+// <form method="post">
+//         <label>or </label>
+// <button name="reg">login</button>
+// </form>
+
+
+
+
+//    </body>
+// </html>
 
 <html>
-<head>
-<title>
+        <head>
+                <title>
                         Register
                 </title>
         </head>
@@ -59,23 +108,76 @@
               <br>
               <button type=" submit " name="register">Register</button>
               <br>
-        </form>
-</div>
+             <a href="login.php">or login  </a>
+
+            </form>
+        
+        </div>
+
+    <?php
+
+       session_start();
 
 
-<?php
-if(isset($_POST['reg'])){
-        header('location:login.php');
-}
-?>
+        if(isset($_POST['register'])){
 
-<form method="post">
-        <label>or </label>
-<button name="reg">login</button>
-</form>
+       include 'connection.php';
+
+       $name=$_POST['Username'];
+       $email=$_POST['email'];
+       $password=$_POST['Password'];
+
+       $insert="insert into user(Username,email,Password) values('$name','$email','$password')";
+
+       # to perform the query above 
+
+       $query=mysqli_query($conn,$insert);
+
+       if($query){
+        echo " Registrated !!";
+        #go to home code 
+        $_SESSION['username']=$name;    # to make in page in my web access to this value 
+        header('location:home.php');
+       }
+       else{
+        echo " Try Again Please";
+       }
+
+     }
+    ?>
+
 
 
 
 
    </body>
 </html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
